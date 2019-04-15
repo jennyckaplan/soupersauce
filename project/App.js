14c4829a3c28534b0e744ba6ea7296caaa3a558c
Jen Kaplan <jenniferckaplan@gmail.com>
2019-04-15 16:14:03 -0400

    implemented routing between pages

--- project/App.js
+++ project/App.js
@@ -7,28 +7,39 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, Text, View, Dimensions} from 'react-native';
+import {Platform, Text, View, TouchableOpacity, Dimensions} from 'react-native';
 import styles from './Styles.js';
+import MainMenu from './MainMenu.js';
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
 type Props = {};
-export default class App extends Component<Props> {
-
+class App extends Component<Props> {
   render() {
+    const {navigate} = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Souper Sauce</Text>
         <View style={styles.connectContainer}>
           <Text style={styles.ready}>Ready to cook?</Text>
-          <View style={styles.connect}>
+          <TouchableOpacity style={styles.connect} onPress={()=> {navigate('Main')}}>
             <Text style={styles.connectText}>Connect</Text>
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
 
-const width = Dimensions.get('window').width;
 
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: App
+  },
+  Main: {
+    screen: MainMenu
+  }
+});
+
+export default createAppContainer(AppNavigator);
 
 
