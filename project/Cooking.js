423258185c5626a7d8abd209a8d2087e01e8014c
Tessa Franzen <tessafranzen@Tessas-MBP-184.fios-router.home>
2019-04-29 13:47:16 -0400

    added stop button in manual

--- project/Cooking.js
+++ project/Cooking.js
@@ -3,6 +3,8 @@ import styles from './Styles';
 import {Text, View, TouchableOpacity, Image, Alert} from 'react-native';
 import CookingIcon from './img/spin.png';
 import AnimatedEllipsis from 'react-native-animated-ellipsis';
+import BleManager from 'react-native-ble-manager';
+import { stringToBytes } from 'convert-string';
 
 class Cooking extends React.Component {
   constructor(props) {
@@ -22,6 +24,33 @@ class Cooking extends React.Component {
             fontFamily: 'Thonburi',
           },
     };
+    stop(){
+    
+      var id = '070FC77C-181A-4F7D-8A81-04DB71C6541B'
+      var service = 'FFE0';
+      var serviceTX = 'ffe1';
+      setTimeout(() => {
+              BleManager.retrieveServices(id).then((peripheralInfo) => {  
+                setTimeout(() => {
+                  BleManager.startNotification(id, service, serviceTX).then(() => {
+                    console.log('Started notification on ' + id);
+                    setTimeout(() => {
+                      BleManager.writeWithoutResponse(id, service, serviceTX, stringToBytes("S")).then(() => {
+                        console.log('Sent message');
+                      });
+  
+                    }, 500);
+                  }).catch((error) => {
+                    console.log('Notification error', error);
+                  });
+                }, 200);
+              });
+  
+            }, 900);
+  
+        const {navigate} = this.props.navigation;
+        navigate('Manual');
+      }
     render() {
     const { navigation } = this.props;
     var timer = navigation.getParam('timer');
@@ -36,6 +65,9 @@ class Cooking extends React.Component {
             </Text>
             <AnimatedEllipsis style={styles.ellipsis} />
             <Image style={styles.cookingSpinningIcon} source={CookingIcon}></Image>
+            <TouchableOpacity style={styles.stopButton} onPress={()=> this.stop()}>
+                    <Text style={styles.startText}>Stop</Text>
+            </TouchableOpacity>
           </View>
         );
     }
