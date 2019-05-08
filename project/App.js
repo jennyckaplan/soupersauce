f4d0800a117fc1d9796fab1ab78cb300a9b4a833
Jen Kaplan <jenniferckaplan@gmail.com>
2019-05-08 14:20:29 -0400

    app changes

--- project/App.js
+++ project/App.js
@@ -7,6 +7,9 @@
  */
 
 import React, {Component} from 'react';
+import {Text, Image, TouchableOpacity, NativeAppEventEmitter,
+  NativeEventEmitter,	
+  NativeModules,AppState, View} from 'react-native';
 import {Text, Image, TouchableOpacity, View} from 'react-native';
 import styles from './Styles.js';
 import MainMenu from './MainMenu.js';
@@ -15,8 +18,13 @@ import Cooking from './Cooking.js';
 import Chef from './img/chef.png';
 import Recipes from './Recipes.js';
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import BleManager from 'react-native-ble-manager';
+import { stringToBytes } from 'convert-string';
 
 type Props = {};
+const BleManagerModule = NativeModules.BleManager;
+const bleManagerEmitter = new NativeEventEmitter(BleManagerModule);
+
 class App extends Component<Props> {
   static navigationOptions = {
     title: 'Souper Sauce',
@@ -32,11 +40,104 @@ class App extends Component<Props> {
     },
   };
 
+  constructor(){	
+    super()	
+
+     this.state = {	
+      scanning:false,	
+      peripherals: new Map(),	
+      appState: '',	
+      item: '',	
+      text: '',	
+    }	
+
+     this.handleDiscoverPeripheral = this.handleDiscoverPeripheral.bind(this);	
+    this.handleStopScan = this.handleStopScan.bind(this);	
+    this.handleUpdateValueForCharacteristic = this.handleUpdateValueForCharacteristic.bind(this);	
+    this.handleDisconnectedPeripheral = this.handleDisconnectedPeripheral.bind(this);	
+    this.handleAppStateChange = this.handleAppStateChange.bind(this);	
+  }	
+
+   componentDidMount() {	
+    AppState.addEventListener('change', this.handleAppStateChange);	
+
+     BleManager.start({showAlert: false});	
+
+     this.handlerDiscover = bleManagerEmitter.addListener('BleManagerDiscoverPeripheral', this.handleDiscoverPeripheral );	
+    this.handlerStop = bleManagerEmitter.addListener('BleManagerStopScan', this.handleStopScan );	
+    this.handlerDisconnect = bleManagerEmitter.addListener('BleManagerDisconnectPeripheral', this.handleDisconnectedPeripheral );	
+    this.handlerUpdate = bleManagerEmitter.addListener('BleManagerDidUpdateValueForCharacteristic', this.handleUpdateValueForCharacteristic );	
+
+
+   }	
+
+   handleAppStateChange(nextAppState) {	
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {	
+      console.log('App has come to the foreground!')	
+      BleManager.getConnectedPeripherals([]).then((peripheralsArray) => {	
+        console.log('Connected peripherals: ' + peripheralsArray.length);	
+      });	
+    }	
+    this.setState({appState: nextAppState});	
+  }	
+
+   componentWillUnmount() {	
+    this.handlerDiscover.remove();	
+    this.handlerStop.remove();	
+    this.handlerDisconnect.remove();	
+    this.handlerUpdate.remove();	
+  }	
+
+   handleDisconnectedPeripheral(data) {	
+    let peripherals = this.state.peripherals;	
+    let peripheral = peripherals.get(data.peripheral);	
+    if (peripheral) {	
+      peripheral.connected = false;	
+      peripherals.set(peripheral.id, peripheral);	
+      this.setState({peripherals});	
+    }	
+    console.log('Disconnected from ' + data.peripheral);	
+  }	
+
+   handleUpdateValueForCharacteristic(data) {	
+    console.log('Received data from ' + data.peripheral + ' characteristic ' + data.characteristic, data.value);	
+  }	
+
+   handleStopScan() {	
+    console.log('Scan is stopped');	
+    this.setState({ scanning: false });	
+  }	
+
+   startScan() {	
+    if (!this.state.scanning) {	
+      this.setState({peripherals: new Map()});	
+      BleManager.scan([], 3, true).then((results) => {	
+        console.log('Scanning...');	
+        this.setState({scanning:true});	
+      });	
+    }	
+  }	
+
+   handleDiscoverPeripheral(peripheral){	
+    var peripherals = this.state.peripherals;	
+    if (peripheral.name == "DSD TECH"){	
+      console.log('Got ble peripheral', peripheral);	
+      peripherals.set(peripheral.id, peripheral);	
+      this.setState({ peripherals });	
+      this.setState({item :peripheral.id});	
+      BleManager.connect(peripheral.id);	
+      this.setState({ scanning: false });	
+      const {navigate} = this.props.navigation;	
+      navigate('Main');	
+
+     }	
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     return (
       <View style={styles.appContainer}>
-        <TouchableOpacity style={styles.connect} onPress={()=> {navigate('Main')}}>
+        <TouchableOpacity style={styles.connect} onPress={()=> this.startScan()}>
           <Text style={styles.connectText}>Start cooking!</Text>
           <Image style={styles.chefIcon} source={Chef} />
         </TouchableOpacity>
