1ee25dd85d214c11bc59aa5365bfb84ae25905a8
Jen Kaplan <jennifer_c_kaplan@brown.edu>
2019-05-12 11:39:49 -0400

    Revert "Merging"

--- project/Manual.js
+++ project/Manual.js
@@ -5,8 +5,6 @@ import whiskIcon from './img/Whisk.png';
 import thermo from './img/thermo.png';
 import timer from './img/timer.png';
 import Slider from '@react-native-community/slider';
-import BleManager from 'react-native-ble-manager';
-import { stringToBytes } from 'convert-string';
 
 const labels = {
   0: "Off",
@@ -39,45 +37,8 @@ class Manual extends React.Component {
           },
     };
 
-    sendInfo(){	
-
-      var id = '070FC77C-181A-4F7D-8A81-04DB71C6541B'	
-     var service = 'FFE0';	
-     var serviceTX = 'ffe1';	
-     var serviceRX = 'ffe1';	
-     var timer = this.state.timer *60;	
-     setTimeout(() => {	
- 
-              BleManager.retrieveServices(id).then((peripheralInfo) => {	
- 
-                console.log('here: ', peripheralInfo);	
- 
-                setTimeout(() => {	
-                 BleManager.startNotification(id, service, serviceTX).then(() => {	
-                   console.log('Started notification on ' + id);	
-                   setTimeout(() => {	
-                     BleManager.writeWithoutResponse(id, service, serviceTX, stringToBytes("T"+timer.toString()+"AW"+this.state.whisk.toString()+"H"+this.state.temperature.toString()+"G")).then(() => {	
-                       console.log('Sent message');	
-                     });	
- 
-                    }, 500);	
-                 }).catch((error) => {	
-                   console.log('Notification error', error);	
-                 });	
-               }, 200);	
-             });	
- 
-            }, 900);	
- 
-        const {navigate} = this.props.navigation;	
-       console.log("timer"+this.state.timer);	
-       navigate('Cooking',{	
-         timer: this.state.timer,	
-       });	
-     }
-
     render() {
-        //const {navigate} = this.props.navigation;
+        const {navigate} = this.props.navigation;
         return (
             <View style={styles.manualContainer}>
                 <View style={styles.whiskSpeed} >
@@ -122,7 +83,7 @@ class Manual extends React.Component {
                     onValueChange={value => this.setState({timer: value})}
                   />
                  <Text style={styles.whiskSpeedText}>{this.state.timer} minutes</Text>
-                 <TouchableOpacity style={styles.startButton} onPress={()=> this.sendInfo()}>
+                 <TouchableOpacity style={styles.startButton} onPress={()=> {navigate('Cooking')}}>
                     <Text style={styles.startText}>Start</Text>
                 </TouchableOpacity>
             </View>
